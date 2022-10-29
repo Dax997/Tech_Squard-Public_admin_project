276ae1648f62d873ad8f24df6a6cc768370cc65c
Dax997 <dkmbulaheni@gmail.com>
2022-10-29 19:25:03 +0200

    database creation

--- GovernmentPortal/Operations/admin.py
+++ GovernmentPortal/Operations/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
-
+from . models import CabinetMember, Project, PublicSector, PublicUser,Municipality, Government,PublicSector
 # Register your models here.
+
+@admin.register(CabinetMember)
+class CabinetMemberAdmin(admin.ModelAdmin):
+    fields = ('user', 'email', 'role','address','office_contact_number')
+    list_display = ('user', 'email', 'role','address','office_contact_number')
+    ordering = ('role',)
+
+@admin.register(PublicUser)
+class PublicUserAdmin(admin.ModelAdmin):
+    fields = ('user','email', 'address','emplyment_status')
+    list_display = ('user','email', 'address','emplyment_status')
+    ordering = ('email',)
+
+@admin.register(PublicSector)
+class PublicSectorAdmin(admin.ModelAdmin):
+    fields = ('name', 'year_budget', 'Head_minister')
+    list_display = ('name', 'year_budget')
+    ordering = ('name',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    fields = ('name', 'sector', 'budget','project_manager')
+    list_display = ('name', 'sector', 'budget','project_manager')
+    ordering = ('name',)
+
+@admin.register(Government)
+class GovernmentAdmin(admin.ModelAdmin):
+    fields = ('political_party', 'year_budget', 'municipality')
+    list_display = ('political_party', 'year_budget',)
+    ordering = ('political_party',)
+
+@admin.register(Municipality)
+class MunicipalityAdmin(admin.ModelAdmin):
+    fields = ('name', 'location', 'head_officer','contact_number')
+    list_display = ('name', 'location','contact_number')
+    ordering = ('name',)
+
--- GovernmentPortal/Operations/models.py
+++ GovernmentPortal/Operations/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class PublicUser(models.Model):
+    user = models.OneToOneField(User,null = True, on_delete=models.CASCADE)
+    EmploymentStatus = (
+        ('Unemplyed', 'Unemplyed'),
+        ('Employed', 'Employed'),
+    )
+    address = models.CharField(max_length=225, blank=True, null=True)
+    email = models.EmailField(max_length=225)
+    emplyment_status = models.CharField(max_length=225, blank=False, choices=EmploymentStatus)
+    
+    
+class CabinetMember(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=225)
+    role = models.CharField(max_length=100, blank=False, null=False)
+    address = models.CharField(max_length=225, blank=True, null=True)
+    office_contact_number = models.IntegerField( null=True, blank=False)
+    
+    def __str__(self):
+        return self.role
+    
+class Municipality(models.Model):
+    name = models.CharField(max_length=225, blank=False, null=False)
+    location = models.CharField(max_length= 100, blank=False, null=False)
+    Head_officer = models.ForeignKey(User, on_delete=models.SET_NULL, blank=False, null=True)
+    contact_number = models.IntegerField( null=True, blank=False)
+    
+    def __str__(self):
+        return self.name
+    
+class Government(models.Model):
+    political_party = models.CharField(max_length = 200, blank=False, null=False)
+    year_budget = models.CharField(max_length=100, blank=False, null=True)
+    Municipality = models.ForeignKey(Municipality,null=True, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.political_party
+    
+class PublicSector(models.Model):
+    name = models.CharField(max_length=225, blank=False, null=False)
+    year_budget = models.CharField(max_length=100, blank=True, null=True)
+    head_minister = models.ForeignKey(CabinetMember, null=True, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.name
+    
+class Project(models.Model):
+    name = models.CharField(max_length=225, blank=False, null=False)
+    sector = models.ForeignKey(PublicSector, on_delete=models.CASCADE)
+    budget = models.DecimalField(decimal_places = 2, max_digits=20)
+    project_manager = models.CharField(max_length=255, blank=False, null=False)
+    
+    def __str__(self):
+        return self.name
+    
